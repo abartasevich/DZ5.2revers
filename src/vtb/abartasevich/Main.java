af926d2be2a03a1085e7d409b26da7b7cc620857
Andrei <abartasevich@vtb.ru>
2020-12-13 22:23:09 +0300

    rere

--- src/vtb/abartasevich/Main.java
+++ src/vtb/abartasevich/Main.java
@@ -8,15 +8,19 @@ public class Main {
         for (int i = 0; i < myArray1.length; i++) {
             System.out.println(myArray1[i]);
         }
-        int[] myArray2 = new int[myArray1.length]; //создаем второй массив равный по длине первому
-        int z = myArray1.length - 1; //создаем переменную равную номеру последней ячейки в первом массиве
-        for (int k = 0; k < myArray2.length; k++) { //заполняем второй массив
+        int[] myArray2 = new int[myArray1.length];
+        //создаем второй массив равный по длине первому
+        int z = myArray1.length - 1;
+        //создаем переменную равную номеру последней ячейки в первом массиве
+        for (int k = 0; k < myArray2.length; k++) {
+            //заполняем второй массив
             int m = myArray1[z];
             myArray2[k] = m;
             z--;
             System.out.println();
         }
-        int j = 0; //Копируем значения второго массива в первый
+        int j = 0;
+        //Копируем значения второго массива в первый
         for (int v = 0; v < myArray1.length; v++) {
             int l = myArray2[j];
             myArray1[v] = l;
